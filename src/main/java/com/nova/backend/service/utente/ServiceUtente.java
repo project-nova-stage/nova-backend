d2d5c8d8f2032b7d53b3aa28b40863b8f7b48b30
contegaetano06-svg <contegaetano06@gmail.com>
2026-03-29 20:41:42 +0200

    Creazione del service utente con la funzione di registrazione(per far registrare l'utente) ho segnato le cose da modificare es: DTO. Vedendo che i dto erano in fase di sviluppo, e i repository non sono stati ancora caricati, ho segnato dove modificare i nomi delle variabili(o oggetti).

--- src/main/java/com/nova/backend/service/utente/ServiceUtente.java
+++ src/main/java/com/nova/backend/service/utente/ServiceUtente.java
@@ -3,10 +3,12 @@ package com.nova.backend.service.utente;
 import com.nova.backend.model.utente.Ruolo;
 import com.nova.backend.model.utente.TipoCliente;
 import com.nova.backend.model.utente.Utente;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class ServiceUtente {
     private final UtenteRepository utenteRepository;
     private final PasswordEncoder passwordEncoder;
